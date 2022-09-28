49f32d825b709e6ca680e6333ddf94c8129b6029
Delakroa <katon1986@gmail.com>
2022-09-28 16:58:16 +0300

    Архивирование директории с файлами

--- trening_zip.py
+++ trening_zip.py
@@ -2,19 +2,27 @@ import os
 import zipfile
 
 
-def main_data():
-    """Получаем список файлов из дериктории"""
-    file_list = os.listdir("d:\\инстр по линии")
-    return file_list
+def go_to_directory():
+    """Переходим в директорию"""
+    os.chdir("d:\\инстр по линии")
+    print(os.listdir())
 
 
 def data_zip():
     """Архивируем файлы"""
-    data = zipfile.ZipFile("d:\\инстр по линии\\jungle.zip", "w")
-    data.write("d:\\инстр по линии\\jungle.zip", compress_type=zipfile.ZIP_DEFLATED)
+    # Обращаемся к модулю для архивации дальнейших записываемых данных
+    zip_file = zipfile.ZipFile("d:\\инстр по линии\\archive.zip", "w")
+    # Записываем в архив файл "Пример файла.docx"
+    for folder, sub_folder, files in os.walk("d:\\инстр по линии"):
+        for file in files:
+            if file.endswith(('.pdf', 'doc')):  # Метод возвращает True, если строка заканчивается указанным значением.
+                zip_file.write(os.path.join(folder, file),
+                               os.path.relpath(os.path.join(folder, file), "d:\\инстр по линии"),
+                               compress_type=zipfile.ZIP_DEFLATED)
 
-    data.close
+    zip_file.close()
 
 
-print(main_data())
+go_to_directory()
 data_zip()
+print(os.listdir())
