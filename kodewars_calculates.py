311c914d2d69be57d2aac864eda279b89d17a3e8
Delakroa <katon1986@gmail.com>
2022-09-28 20:58:33 +0300

    Время 20-57 ничего в голову не идёт. Нужно переделать эту хрень

--- kodewars_calculates.py
+++ kodewars_calculates.py
@@ -7,20 +7,34 @@
 #     def basic_test_cases():
 #         test.assert_equals(find_average([1, 2, 3]), 2)
 
-from statistics import mean
+# from statistics import mean
 
 
-def calculates_the_average(*args: int):
-    """Среднее вычисление массива"""
-    try:
-        average_a = mean([*args])
+# На codewars данный вариант не работает
+# def find_average(*args: int):
+#     """Среднее вычисление массива"""
+#     try:
+#         average_a = mean([*args])
+#
+#         return average_a
+#
+#     except statistics.StatisticsError:
+#         return 0
+#
+#
+# print(find_average(123, 43, 32))
 
-        return average_a
 
-    except statistics.StatisticsError:
-        return 0
+# Делаем второй вариант
+
+def find_average(number):
+    count = 0
+    for numbers in range(number):
+        count += numbers
+    return count
 
 
-print(calculates_the_average(123, 43, 32))
+print(find_average([1, 2, 3, 4, 5]))
+
 
 
